cef69473ee32ac99c7872054fbad69662653a5d0
fsanchezcgs <fsanchez19962@iesjoanramis.org>
2024-01-09 16:59:52 +0100

    09-01-2024 indexcss

--- blog-dwec/src/Inicio.js
+++ blog-dwec/src/Inicio.js
@@ -1,6 +1,22 @@
+import { useState } from "react";
+
 const Inicio = () => {
+
+    const [blogs, setBlogs] = useState([
+        { titulo: "Mi nueva web", body: "texto para mostrar", autor: "Joan", id:1},
+        { titulo: "Fiesta del Ramis", body: "mucha paella", autor: "Enrique", id:2},
+        { titulo: "Tecnicas de estudio", body: "echarle horas", autor: "Irene", id:3}
+    ]);
+
     return ( 
-        <h2>Pagina de inicio</h2>
+        <div className="home">
+            {blogs.map((blog)=>{
+                return <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.titulo}</h2>
+                    <p>Escrito por {blog.autor}</p>
+                </div>
+            })}
+        </div>
      );
 }
  
